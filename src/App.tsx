41f24e46f2a7ed65f840433058fabb57c879c85d
Giovana Napoli <giovana.ap.napoli@outlook.com>
2024-05-16 13:51:14 -0300

    feat: mobile first

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,13 @@ import "./index.css";
 function App() {
   return (
     <div className="flex h-full w-full justify-center">
-      <div className="flex flex-col justify-center w-3/5">
+      <div className="flex flex-col justify-center w-9/12 md:w-97/12">
         <SocialBar />
         <main className="mt-16">
-          <div id="about_me" className="flex flex-row gap-5 items-start ">
+          <section
+            id="about"
+            className="flex flex-col gap-5 items-start md:flex-row "
+          >
             <div>
               <img
                 src="./friren.jpg"
@@ -18,31 +21,34 @@ function App() {
               />
             </div>
             <div className="flex flex-col gap-5">
-              <section className="flex flex-col gap-2">
-                <h1 className="text-4xl font-aptos-black">
-                  Olá, sou a Giovana 👋
-                </h1>
-                <p>
-                  Sou desenvolvedora front-end pleno, atualmente trabalhando na{" "}
-                  <CommonLink link="https://modalgr.io/" text="ModalGR" />.
-                </p>
-                <p>Anteriormente trabalhei em:</p>
-                <ul className="list-disc list-inside">
-                  <li>
-                    Estágiaria front-end na{" "}
-                    <CommonLink
-                      link="https://www.otimaideia.com.br/"
-                      text="Ótima Ideia"
-                    />
-                  </li>
-                </ul>
-              </section>
+              <h1 className="text-4xl font-aptos-black">
+                Olá, sou a Giovana 👋
+              </h1>
+              <p>
+                Sou desenvolvedora front-end pleno, atualmente trabalhando na{" "}
+                <CommonLink link="https://modalgr.io/" text="ModalGR" />.
+              </p>
+              <p>Anteriormente trabalhei em:</p>
+              <ul className="list-disc list-inside">
+                <li>
+                  Estágiaria front-end na{" "}
+                  <CommonLink
+                    link="https://www.otimaideia.com.br/"
+                    text="Ótima Ideia"
+                  />
+                </li>
+              </ul>
             </div>
-          </div>
-          <section id="courses" className="flex gap-5 mt-10">
-            <div className="size-28" />
+          </section>
+          <section
+            id="courses"
+            className="flex flex-col md:flex-row gap-5 mt-10"
+          >
+            <div className="size-0 md:size-28" />
             <div>
-              <h2 className="text-2xl font-aptos-black">Cursos e formações</h2>
+              <h2 className="text-2xl font-aptos-black">
+                Cursos e formações 🎓
+              </h2>
               <ul className="list-disc list-inside">
                 {courses.map(({ name, link }) => (
                   <li key={name}>
@@ -56,13 +62,12 @@ function App() {
               </ul>
             </div>
           </section>
-          <section id="projects" className="flex gap-5">
-            <div className="size-28" />
-            <div>
-              <h2 className="text-2xl font-aptos-black">Projetos</h2>
-            </div>
-          </section>
         </main>
+        <div className="flex justify-center mt-10">
+          <span className="text-sm font-aptos-semibold">
+            Ainda estou desenvolvendo esse site 🫣
+          </span>
+        </div>
         <footer className="backdrop-blur sticky bottom-0 mt-16">
           <span className="text-sm">
             © 2024 Giovana Aparecida Napoli da Silva.
