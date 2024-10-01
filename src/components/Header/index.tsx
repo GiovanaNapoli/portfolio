49535e083b9d78667ccf79c65ba2439658686f48
Giovana Napoli <giovana.ap.napoli@outlook.com>
2024-10-01 15:46:37 -0300

    fix: remove unused component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,4 @@
 import { Link } from "react-router-dom";
-import IconWrapper from "../iconWrapper";
 import { RiMoonLine } from "react-icons/ri";
 
 interface HeaderProps {
@@ -13,32 +12,30 @@ const Header = ({ switchMode }: HeaderProps) => {
   ];
 
   return (
-    <IconWrapper className="fill-zinc-500 hover:fill-zinc-700 dark:fill-white dark:hover:fill-gray-400 transition-colors">
-      <header className="fixed top-0 backdrop-blur inset-x-0 px-4 py-6 print:hidden">
-        <nav className="flex flex-row justify-center items-center">
-          <div className="flex flex-row items-center justify-between w-full max-w-[710px]">
-            <Link to={"/"} className="text-lg font-aptos-semibold">
-              Giovana Aparecida
-            </Link>
-            <div className="flex flex-row justify-center items-center gap-5">
-              {routes.map((route, index) => (
-                <Link
-                  key={index}
-                  to={route.to}
-                  className="font-aptos-semibold cursor-pointer"
-                >
-                  {route.label}
-                </Link>
-              ))}
+    <header className="fixed top-0 backdrop-blur inset-x-0 px-4 py-6 print:hidden">
+      <nav className="flex flex-row justify-center items-center">
+        <div className="flex flex-row items-center justify-between w-full max-w-[710px]">
+          <Link to={"/"} className="text-lg font-aptos-semibold">
+            Giovana Aparecida
+          </Link>
+          <div className="flex flex-row justify-center items-center gap-5">
+            {routes.map((route, index) => (
+              <Link
+                key={index}
+                to={route.to}
+                className="font-aptos-semibold cursor-pointer"
+              >
+                {route.label}
+              </Link>
+            ))}
 
-              <button onClick={switchMode}>
-                <RiMoonLine size={24} />
-              </button>
-            </div>
+            <button onClick={switchMode}>
+              <RiMoonLine size={24} />
+            </button>
           </div>
-        </nav>
-      </header>
-    </IconWrapper>
+        </div>
+      </nav>
+    </header>
   );
 };
 
