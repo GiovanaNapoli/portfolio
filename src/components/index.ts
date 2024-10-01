0c58e51190f990ac81a320c33cd1bbd132946668
Giovana Aparecida Napoli da Silva <32879408+GiovanaNapoli@users.noreply.github.com>
2024-10-01 15:42:26 -0300

    fix: fix import name

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,3 @@
 export { default as Container } from "./container";
-export { default as Footer } from "./footer";
-export { default as Header } from "./header";
+export { default as Footer } from "./Footer";
+export { default as Header } from "./Header";
